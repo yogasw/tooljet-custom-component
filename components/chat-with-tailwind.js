fd31a697e36a9dafb81a63b1e567b0716015781f
yogasw <ariokidev@gmail.com>
2023-08-18 23:35:20 +0000

    Deploying to gh-pages from @ yogasw/tooljet-custom-component@780f9fab22a248520307ccf9c9d3af52ee727437 🚀

---
+++ components/chat-with-tailwind.js
@@ -0,0 +1,141 @@
+import * as React from "https://esm.sh/react";
+const Head = () => /* @__PURE__ */ React.createElement("head", null, /* @__PURE__ */ React.createElement("link", {
+  href: "https://cdn.jsdelivr.net/npm/tailwindcss@latest/dist/tailwind.min.css",
+  rel: "stylesheet"
+}));
+const posts = [
+  {
+    id: 1,
+    title: "Boost your conversion rate",
+    href: "#",
+    description: "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
+    date: "Mar 16, 2020",
+    datetime: "2020-03-16",
+    category: {title: "Marketing", href: "#"},
+    author: {
+      name: "Michael Foster",
+      role: "Co-Founder / CTO",
+      href: "#",
+      imageUrl: "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+    }
+  },
+  {
+    id: 1,
+    title: "Boost your conversion rate",
+    href: "#",
+    description: "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
+    date: "Mar 16, 2020",
+    datetime: "2020-03-16",
+    category: {title: "Marketing", href: "#"},
+    author: {
+      name: "Michael Foster",
+      role: "Co-Founder / CTO",
+      href: "#",
+      imageUrl: "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+    }
+  }
+];
+const ChatWithTailwind = ({data, updateData, runQuery}) => /* @__PURE__ */ React.createElement("div", null, /* @__PURE__ */ React.createElement(Head, null), /* @__PURE__ */ React.createElement("div", {
+  className: "flex flex-col items-center justify-center w-screen min-h-screen bg-gray-100 text-gray-800 p-10"
+}, /* @__PURE__ */ React.createElement("div", {
+  className: "flex flex-col flex-grow w-full max-w-xl bg-white shadow-xl rounded-lg overflow-hidden"
+}, /* @__PURE__ */ React.createElement("div", {
+  className: "flex flex-col flex-grow h-0 p-4 overflow-auto"
+}, /* @__PURE__ */ React.createElement("div", {
+  className: "flex w-full mt-2 space-x-3 max-w-xs"
+}, /* @__PURE__ */ React.createElement("div", {
+  className: "flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"
+}), /* @__PURE__ */ React.createElement("div", null, /* @__PURE__ */ React.createElement("div", {
+  className: "bg-gray-300 p-3 rounded-r-lg rounded-bl-lg"
+}, /* @__PURE__ */ React.createElement("p", {
+  className: "text-sm"
+}, "Lorem ipsum dolor sit amet, consectetur adipiscing elit.")), /* @__PURE__ */ React.createElement("span", {
+  className: "text-xs text-gray-500 leading-none"
+}, "2 min ago"))), /* @__PURE__ */ React.createElement("div", {
+  className: "flex w-full mt-2 space-x-3 max-w-xs ml-auto justify-end"
+}, /* @__PURE__ */ React.createElement("div", null, /* @__PURE__ */ React.createElement("div", {
+  className: "bg-blue-600 text-white p-3 rounded-l-lg rounded-br-lg"
+}, /* @__PURE__ */ React.createElement("p", {
+  className: "text-sm"
+}, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.")), /* @__PURE__ */ React.createElement("span", {
+  className: "text-xs text-gray-500 leading-none"
+}, "2 min ago")), /* @__PURE__ */ React.createElement("div", {
+  className: "flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"
+})), /* @__PURE__ */ React.createElement("div", {
+  className: "flex w-full mt-2 space-x-3 max-w-xs ml-auto justify-end"
+}, /* @__PURE__ */ React.createElement("div", null, /* @__PURE__ */ React.createElement("div", {
+  className: "bg-blue-600 text-white p-3 rounded-l-lg rounded-br-lg"
+}, /* @__PURE__ */ React.createElement("p", {
+  className: "text-sm"
+}, "Lorem ipsum dolor sit amet.")), /* @__PURE__ */ React.createElement("span", {
+  className: "text-xs text-gray-500 leading-none"
+}, "2 min ago")), /* @__PURE__ */ React.createElement("div", {
+  className: "flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"
+})), /* @__PURE__ */ React.createElement("div", {
+  className: "flex w-full mt-2 space-x-3 max-w-xs"
+}, /* @__PURE__ */ React.createElement("div", {
+  className: "flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"
+}), /* @__PURE__ */ React.createElement("div", null, /* @__PURE__ */ React.createElement("div", {
+  className: "bg-gray-300 p-3 rounded-r-lg rounded-bl-lg"
+}, /* @__PURE__ */ React.createElement("p", {
+  className: "text-sm"
+}, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ")), /* @__PURE__ */ React.createElement("span", {
+  className: "text-xs text-gray-500 leading-none"
+}, "2 min ago"))), /* @__PURE__ */ React.createElement("div", {
+  className: "flex w-full mt-2 space-x-3 max-w-xs ml-auto justify-end"
+}, /* @__PURE__ */ React.createElement("div", null, /* @__PURE__ */ React.createElement("div", {
+  className: "bg-blue-600 text-white p-3 rounded-l-lg rounded-br-lg"
+}, /* @__PURE__ */ React.createElement("p", {
+  className: "text-sm"
+}, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ")), /* @__PURE__ */ React.createElement("span", {
+  className: "text-xs text-gray-500 leading-none"
+}, "2 min ago")), /* @__PURE__ */ React.createElement("div", {
+  className: "flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"
+})), /* @__PURE__ */ React.createElement("div", {
+  className: "flex w-full mt-2 space-x-3 max-w-xs ml-auto justify-end"
+}, /* @__PURE__ */ React.createElement("div", null, /* @__PURE__ */ React.createElement("div", {
+  className: "bg-blue-600 text-white p-3 rounded-l-lg rounded-br-lg"
+}, /* @__PURE__ */ React.createElement("p", {
+  className: "text-sm"
+}, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.")), /* @__PURE__ */ React.createElement("span", {
+  className: "text-xs text-gray-500 leading-none"
+}, "2 min ago")), /* @__PURE__ */ React.createElement("div", {
+  className: "flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"
+})), /* @__PURE__ */ React.createElement("div", {
+  className: "flex w-full mt-2 space-x-3 max-w-xs ml-auto justify-end"
+}, /* @__PURE__ */ React.createElement("div", null, /* @__PURE__ */ React.createElement("div", {
+  className: "bg-blue-600 text-white p-3 rounded-l-lg rounded-br-lg"
+}, /* @__PURE__ */ React.createElement("p", {
+  className: "text-sm"
+}, "Lorem ipsum dolor sit amet.")), /* @__PURE__ */ React.createElement("span", {
+  className: "text-xs text-gray-500 leading-none"
+}, "2 min ago")), /* @__PURE__ */ React.createElement("div", {
+  className: "flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"
+})), /* @__PURE__ */ React.createElement("div", {
+  className: "flex w-full mt-2 space-x-3 max-w-xs"
+}, /* @__PURE__ */ React.createElement("div", {
+  className: "flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"
+}), /* @__PURE__ */ React.createElement("div", null, /* @__PURE__ */ React.createElement("div", {
+  className: "bg-gray-300 p-3 rounded-r-lg rounded-bl-lg"
+}, /* @__PURE__ */ React.createElement("p", {
+  className: "text-sm"
+}, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ")), /* @__PURE__ */ React.createElement("span", {
+  className: "text-xs text-gray-500 leading-none"
+}, "2 min ago"))), /* @__PURE__ */ React.createElement("div", {
+  className: "flex w-full mt-2 space-x-3 max-w-xs ml-auto justify-end"
+}, /* @__PURE__ */ React.createElement("div", null, /* @__PURE__ */ React.createElement("div", {
+  className: "bg-blue-600 text-white p-3 rounded-l-lg rounded-br-lg"
+}, /* @__PURE__ */ React.createElement("p", {
+  className: "text-sm"
+}, "Lorem ipsum dolor sit.")), /* @__PURE__ */ React.createElement("span", {
+  className: "text-xs text-gray-500 leading-none"
+}, "2 min ago")), /* @__PURE__ */ React.createElement("div", {
+  className: "flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"
+}))), /* @__PURE__ */ React.createElement("div", {
+  className: "bg-gray-300 p-4"
+}, /* @__PURE__ */ React.createElement("input", {
+  className: "flex items-center h-10 w-full rounded px-3 text-sm",
+  type: "text",
+  placeholder: "Type your message…"
+})))));
+export default ChatWithTailwind;
--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-import * as React from "https://esm.sh/react";
+import React, {useState} from "https://esm.sh/react";
 import ReactDOMClient from "https://esm.sh/stable/react-dom/client";
+import {createPortal} from "https://esm.sh/stable/react-dom";
 import DynamicInput from "./components/dynamic-input.js";
 import DateTimeLocal from "./components/date-time-local.js";
 import MyCustomComponent from "./components/my-custom-component.js";
@@ -9,6 +10,18 @@ import EsmSh from "./components/esm-sh.js";
 import Timer from "./components/timer.js";
 import RetejsBasic from "./components/retejs-basic.js";
 import PrimitivesDiagrams from "./components/primitives-diagrams.js";
+import ChatWithTailwind from "./components/chat-with-tailwind.js";
+export const IFrame = ({
+  children,
+  ...props
+}) => {
+  const [contentRef, setContentRef] = useState(null);
+  const mountNode = contentRef?.contentWindow?.document?.body;
+  return /* @__PURE__ */ React.createElement("iframe", {
+    ...props,
+    ref: setContentRef
+  }, mountNode && createPortal(children, mountNode));
+};
 const App = () => {
   const [data, setData] = React.useState({
     value: 'Example |="{{B}}"',
@@ -27,7 +40,10 @@ const App = () => {
   const runQuery = (query) => {
     console.log("run query", query);
   };
-  return /* @__PURE__ */ React.createElement("div", null, /* @__PURE__ */ React.createElement("h1", null, "Basic Primitives Diagrams for React"), /* @__PURE__ */ React.createElement(PrimitivesDiagrams, null), /* @__PURE__ */ React.createElement("h1", null, "Retejs Basic"), /* @__PURE__ */ React.createElement(RetejsBasic, null), /* @__PURE__ */ React.createElement("h1", null, "ReactAutocompleteInput"), /* @__PURE__ */ React.createElement(ReactAutocompleteInput, {
+  return /* @__PURE__ */ React.createElement("div", null, /* @__PURE__ */ React.createElement("h1", null, "Chat Component with Tailwind Css"), /* @__PURE__ */ React.createElement(IFrame, {
+    width: "100%",
+    height: "400"
+  }, /* @__PURE__ */ React.createElement(ChatWithTailwind, null)), /* @__PURE__ */ React.createElement("h1", null, "Basic Primitives Diagrams for React"), /* @__PURE__ */ React.createElement(PrimitivesDiagrams, null), /* @__PURE__ */ React.createElement("h1", null, "Retejs Basic"), /* @__PURE__ */ React.createElement(RetejsBasic, null), /* @__PURE__ */ React.createElement("h1", null, "ReactAutocompleteInput"), /* @__PURE__ */ React.createElement(ReactAutocompleteInput, {
     updateData: (data2) => {
       updateData(data2);
     },
