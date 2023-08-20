69dd2cc36e1fd8fe262f6af9808f8264fd49aa64
Yoga Setiawan <yogainformatika@gmail.com>
2023-08-21 06:11:33 +0700

    auto diagram

--- src/components/react-flow-diagram.jsx
+++ src/components/react-flow-diagram.jsx
@@ -1,5 +1,14 @@
 import React, {useCallback} from 'https://cdn.jsdelivr.net/npm/react@18.2.0/+esm';
-import ReactFlow, {useNodesState, useEdgesState, addEdge} from "https://cdn.jsdelivr.net/npm/reactflow/+esm";
+import ReactFlow, {
+    addEdge,
+    ConnectionLineType,
+    useNodesState,
+    useEdgesState,
+} from "https://cdn.jsdelivr.net/npm/reactflow/+esm";
+import dagre from 'https://cdn.jsdelivr.net/npm/@dagrejs/dagre/+esm';
+
+const dagreGraph = new dagre.graphlib.Graph();
+dagreGraph.setDefaultEdgeLabel(() => ({}));
 
 const HeadReactFlow = () => (
     <head>
@@ -7,122 +16,57 @@ const HeadReactFlow = () => (
     </head>
 );
 
+const getLayoutedElements = (nodes, edges, nodeWidth, nodeHeight, direction = 'LR') => {
+    const isHorizontal = direction === 'LR';
+    dagreGraph.setGraph({rankdir: direction});
 
-const initialNodes = [
-    {
-        id: 'horizontal-1',
-        sourcePosition: 'right',
-        type: 'input',
-        data: {label: 'Input'},
-        position: {x: 0, y: 80},
-    },
-    {
-        id: 'horizontal-2',
-        sourcePosition: 'right',
-        targetPosition: 'left',
-        data: {label: 'A Node'},
-        position: {x: 250, y: 0},
-    },
-    {
-        id: 'horizontal-3',
-        sourcePosition: 'right',
-        targetPosition: 'left',
-        data: {label: 'Node 3'},
-        position: {x: 250, y: 160},
-    },
-    {
-        id: 'horizontal-4',
-        sourcePosition: 'right',
-        targetPosition: 'left',
-        data: {label: 'Node 4'},
-        position: {x: 500, y: 0},
-    },
-    {
-        id: 'horizontal-5',
-        sourcePosition: 'top',
-        targetPosition: 'bottom',
-        data: {label: 'Node 5'},
-        position: {x: 500, y: 100},
-    },
-    {
-        id: 'horizontal-6',
-        sourcePosition: 'bottom',
-        targetPosition: 'top',
-        data: {label: 'Node 6'},
-        position: {x: 500, y: 230},
-    },
-    {
-        id: 'horizontal-7',
-        sourcePosition: 'right',
-        targetPosition: 'left',
-        data: {label: 'Node 7'},
-        position: {x: 750, y: 50},
-    },
-    {
-        id: 'horizontal-8',
-        sourcePosition: 'right',
-        targetPosition: 'left',
-        data: {label: 'Node 8'},
-        position: {x: 750, y: 300},
-    },
-];
+    nodes?.forEach((node) => {
+        dagreGraph.setNode(node.id, {width: nodeWidth, height: nodeHeight});
+    });
 
-const initialEdges = [
-    {
-        id: 'horizontal-e1-2',
-        source: 'horizontal-1',
-        type: 'smoothstep',
-        target: 'horizontal-2',
-        animated: true,
-    },
-    {
-        id: 'horizontal-e1-3',
-        source: 'horizontal-1',
-        type: 'smoothstep',
-        target: 'horizontal-3',
-        animated: true,
-    },
-    {
-        id: 'horizontal-e1-4',
-        source: 'horizontal-2',
-        type: 'smoothstep',
-        target: 'horizontal-4',
-        label: 'edge label',
-    },
-    {
-        id: 'horizontal-e3-5',
-        source: 'horizontal-3',
-        type: 'smoothstep',
-        target: 'horizontal-5',
-        animated: true,
-    },
-    {
-        id: 'horizontal-e3-6',
-        source: 'horizontal-3',
-        type: 'smoothstep',
-        target: 'horizontal-6',
-        animated: true,
-    },
-    {
-        id: 'horizontal-e5-7',
-        source: 'horizontal-5',
-        type: 'smoothstep',
-        target: 'horizontal-7',
-        animated: true,
-    },
-    {
-        id: 'horizontal-e6-8',
-        source: 'horizontal-6',
-        type: 'smoothstep',
-        target: 'horizontal-8',
-        animated: true,
-    },
-];
+    edges?.forEach((edge) => {
+        dagreGraph.setEdge(edge.source, edge.target);
+    });
 
-const ReactFlowDiagram = ({data, updateData, runQuery}) => {
-    const [nodes, _, onNodesChange] = useNodesState(initialNodes);
-    const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
-    const onConnect = useCallback((params) => setEdges((els) => addEdge(params, els)), []);
+    dagre.layout(dagreGraph);
+
+    nodes?.forEach((node) => {
+        const nodeWithPosition = dagreGraph.node(node.id);
+        node.targetPosition = isHorizontal ? 'left' : 'top';
+        node.sourcePosition = isHorizontal ? 'right' : 'bottom';
+
+        // We are shifting the dagre node position (anchor=center center) to the top left
+        // so it matches the React Flow node anchor point (top left).
+        node.position = {
+            x: nodeWithPosition.x - nodeWidth / 2,
+            y: nodeWithPosition.y - nodeHeight / 2,
+        };
+
+        return node;
+    });
+
+    return {nodes, edges};
+};
+
+const ReactFlowDiagram = ({data}) => {
+    const {initialNodes, initialEdges, nodeWidth, nodeHeight} = data;
+    const {nodes: layoutedNodes, edges: layoutedEdges} = getLayoutedElements(
+        initialNodes,
+        initialEdges,
+        nodeWidth,
+        nodeHeight
+    );
+
+    const [nodes, setNodes, onNodesChange] = useNodesState(layoutedNodes);
+    const [edges, setEdges, onEdgesChange] = useEdgesState(layoutedEdges);
+
+    const onConnect = useCallback(
+        (params) =>
+            setEdges((eds) =>
+                addEdge({...params, type: ConnectionLineType.SmoothStep, animated: true}, eds)
+            ),
+        []
+    );
 
     return (
         <div>
@@ -133,11 +77,13 @@ const ReactFlowDiagram = ({data, updateData, runQuery}) => {
                 onNodesChange={onNodesChange}
                 onEdgesChange={onEdgesChange}
                 onConnect={onConnect}
+                connectionLineType={ConnectionLineType.SmoothStep}
                 fitView
-                attributionPosition="bottom-left"
-            />
+                onNodeClick={(event, element) => console.log('click', element)}
+            >
+            </ReactFlow>
         </div>
-    )
+    );
 };
 
 export default ReactFlowDiagram;
