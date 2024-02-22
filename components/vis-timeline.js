57392123902995229d47577660d3536962991a48
yogasw <ariokidev@gmail.com>
2024-02-22 07:31:02 +0000

    Deploying to gh-pages from @ yogasw/tooljet-custom-component@e64256c80ae57d2acebd40658549ff129c9396b3 🚀

--- components/vis-timeline.js
+++ components/vis-timeline.js
@@ -1,30 +1,29 @@
 import React, {useCallback, useState, useEffect, useRef} from "https://cdn.jsdelivr.net/npm/react@18.2.0/+esm";
-import vis from "https://cdn.jsdelivr.net/npm/vis@4.21.0/+esm";
+import {Timeline, DataSet} from "https://cdn.jsdelivr.net/npm/vis-timeline/standalone/+esm";
 import ReactDOM from "https://cdn.jsdelivr.net/npm/react-dom@18.2.0/+esm";
-const Header = () => /* @__PURE__ */ React.createElement("head", null, /* @__PURE__ */ React.createElement("link", {
-  href: "https://cdn.jsdelivr.net/npm/vis@4.21.0/dist/vis.min.css",
-  rel: "stylesheet"
-}));
-function GroupTemplate({group}) {
-  return /* @__PURE__ */ React.createElement("div", null, /* @__PURE__ */ React.createElement("label", null, group.content));
+class GroupTemplate extends React.Component {
+  render() {
+    var {group} = this.props;
+    return /* @__PURE__ */ React.createElement("div", null, /* @__PURE__ */ React.createElement("label", null, group.content));
+  }
 }
-function ItemTemplate({item}) {
-  return /* @__PURE__ */ React.createElement("div", null, /* @__PURE__ */ React.createElement("label", null, item.content));
+class ItemTemplate extends React.Component {
+  render() {
+    var {item} = this.props;
+    return /* @__PURE__ */ React.createElement("div", null, /* @__PURE__ */ React.createElement("label", null, item.content));
+  }
 }
-const VisTimeline = ({data, updateData, runQuery}) => {
-  const ref = useRef(0);
-  var timeline;
-  const {items: _items, groups: _groups} = data ? data : {items: [], groups: []};
-  useEffect(() => {
-    var options = {
-      orientation: "top",
-      maxHeight: 400,
-      start: new Date(),
-      end: new Date(1e3 * 60 * 60 * 24 + new Date().valueOf()),
-      editable: false
+class VisTimeline extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      timeline: null
     };
-    var groups = new vis.DataSet();
-    var items = new vis.DataSet();
+  }
+  initData() {
+    var groups = new DataSet();
+    var items = new DataSet();
+    const {items: _items, groups: _groups} = this.props?.data ? this.props?.data : {items: [], groups: []};
     if (_groups) {
       _groups.forEach((group) => {
         groups.add(group);
@@ -35,12 +34,33 @@ const VisTimeline = ({data, updateData, runQuery}) => {
         items.add(item);
       });
     }
-    var container = document.getElementById("mytimeline");
-    timeline = new vis.Timeline(container, items, groups, options);
-  }, [data]);
-  return /* @__PURE__ */ React.createElement("div", null, /* @__PURE__ */ React.createElement(Header, null), /* @__PURE__ */ React.createElement("div", {
-    id: "mytimeline",
-    ref
-  }));
-};
-export default VisTimeline;
+    return {items, groups};
+  }
+  initTimeline() {
+    var options = {
+      orientation: "top",
+      start: new Date(),
+      end: new Date(1e3 * 60 * 60 * 24 + new Date().valueOf()),
+      editable: false
+    };
+    const {items, groups} = this.initData();
+    var container = document.getElementById("visualization");
+    this.timeline = new Timeline(container, items, groups, options);
+  }
+  componentDidMount() {
+    return this.initTimeline();
+  }
+  componentDidUpdate() {
+    this.timeline.destroy();
+    return this.initTimeline();
+  }
+  render() {
+    return /* @__PURE__ */ React.createElement("div", null, /* @__PURE__ */ React.createElement("div", {
+      id: "visualization"
+    }));
+  }
+}
+;
+import {createRoot} from "https://cdn.jsdelivr.net/npm/react-dom/+esm";
+const ConnectedComponent = Tooljet.connectComponent(VisTimeline);
+createRoot(document.body).render(/* @__PURE__ */ React.createElement(ConnectedComponent, null));
