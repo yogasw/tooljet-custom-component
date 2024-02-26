c25d71032ae6cade6fb47d471bfc68ec7fdf0216
yogasw <ariokidev@gmail.com>
2024-02-26 04:39:45 +0000

    Deploying to gh-pages from @ yogasw/tooljet-custom-component@25b08c23e81d69c39037631691dbcebf8b1b1959 🚀

--- components/dynamic-input.js
+++ components/dynamic-input.js
@@ -1,6 +1,6 @@
 import * as React from "https://cdn.jsdelivr.net/npm/react@18.2.0/+esm";
-import TextField from "https://cdn.jsdelivr.net/npm/@mui/material/TextField/+esm";
-import Typography from "https://cdn.jsdelivr.net/npm/@mui/material/Typography/+esm";
+import TextField from "https://cdn.jsdelivr.net/npm/@mui/material@5.15.0/TextField/+esm";
+import Typography from "https://cdn.jsdelivr.net/npm/@mui/material@5.15.0/Typography/+esm";
 import {FiPlus, FiTrash} from "https://cdn.jsdelivr.net/npm/react-icons/fi/+esm";
 const DynamicInput = ({data, updateData, runQuery}) => {
   const {useEffect} = React;
--- components/my-custom-component.js
+++ components/my-custom-component.js
@@ -1,6 +1,6 @@
 import * as React from "https://cdn.jsdelivr.net/npm/react@18.2.0/+esm";
-import Button from "https://cdn.jsdelivr.net/npm/@mui/material/Button/+esm";
-import Container from "https://cdn.jsdelivr.net/npm/@mui/material/Container/+esm";
+import Button from "https://cdn.jsdelivr.net/npm/@mui/material@5.15.0/Button/+esm";
+import Container from "https://cdn.jsdelivr.net/npm/@mui/material@5.15.0/Container/+esm";
 const MyCustomComponent = ({data, updateData, runQuery}) => /* @__PURE__ */ React.createElement(Container, null, /* @__PURE__ */ React.createElement("h1", null, data?.title), /* @__PURE__ */ React.createElement(Button, {
   color: "primary",
   variant: "outlined",
