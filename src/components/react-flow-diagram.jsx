10e4d523b2fc9c6e2eeb6bf8d960fb3732fc4f09
Yoga Setiawan <yogainformatika@gmail.com>
2024-12-13 09:23:12 +0700

    improve color

--- src/components/react-flow-diagram.jsx
+++ src/components/react-flow-diagram.jsx
@@ -134,7 +134,7 @@ const DetailIntent = (data) => {
         <div className="bg-white shadow overflow-hidden sm:rounded-lg">
             <div className="px-4 py-5 sm:px-6">
                 <h3 className="text-lg leading-6 font-medium text-gray-900">{displayName}</h3>
-                <p className="mt-1 max-w-2xl text-sm text-gray-500">
+                <p className="mt-1 max-w-2xl text-sm text-blue-500">
                     <a target="_blank" rel="noopener noreferrer" href={url}>Link Dialogflow</a>
                 </p>
             </div>
