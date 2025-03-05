9fcd6cc849a2dca369ddfa684a9254f764c71e58
yogasw <ariokidev@gmail.com>
2025-03-05 03:14:35 +0000

    Deploying to gh-pages from @ yogasw/tooljet-custom-component@5d8b2e7bb80e8c1b7ec6f410945ae6b030663c0d 🚀

--- components/dynamic-input.js
+++ components/dynamic-input.js
@@ -1,15 +1,26 @@
 import * as React from "https://cdn.jsdelivr.net/npm/react@18.2.0/+esm";
-import TextField from "https://cdn.jsdelivr.net/npm/@mui/material@5.15.0/TextField/+esm";
-import Typography from "https://cdn.jsdelivr.net/npm/@mui/material@5.15.0/Typography/+esm";
 import {FiPlus, FiTrash} from "https://cdn.jsdelivr.net/npm/react-icons@5.4.0/fi/+esm";
+const HeadTailwind = () => /* @__PURE__ */ React.createElement("head", null, /* @__PURE__ */ React.createElement("link", {
+  href: "https://cdn.jsdelivr.net/npm/daisyui@5",
+  rel: "stylesheet",
+  type: "text/css"
+}), /* @__PURE__ */ React.createElement("script", {
+  src: "https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"
+}));
 const DynamicInput = ({data, updateData, runQuery}) => {
   const {useEffect} = React;
   const ALPHA_NUMERIC_DASH_REGEX = /^[a-zA-Z0-9\_]+$/;
   const inputList = () => {
     if (!data?.inputList) {
-      return [{key: "", value: ""}];
+      return [{key: "test", value: "test"}];
     }
     return data?.inputList;
+  };
+  const adjustHeightTextarea = (textarea) => {
+    if (textarea) {
+      textarea.style.height = "auto";
+      textarea.style.height = `${textarea.scrollHeight}px`;
+    }
   };
   const setInputList = (inputList2) => {
     updateData({inputList: inputList2});
@@ -40,25 +51,21 @@ const DynamicInput = ({data, updateData, runQuery}) => {
   }, [data?.reload === true]);
   if (data?.reload === true)
     return /* @__PURE__ */ React.createElement(React.Fragment, null);
-  return /* @__PURE__ */ React.createElement("div", null, /* @__PURE__ */ React.createElement(Typography, {
+  return /* @__PURE__ */ React.createElement("div", null, /* @__PURE__ */ React.createElement(HeadTailwind, null), /* @__PURE__ */ React.createElement("p", {
     style: {marginLeft: 10},
-    variant: "caption",
-    display: "block",
-    gutterBottom: true
+    className: "italic"
   }, "*key only a-z, 0-9 and _"), inputList()?.map((x, i) => {
     return /* @__PURE__ */ React.createElement("div", {
       style: stylesDynamicInput.box,
       key: i
-    }, /* @__PURE__ */ React.createElement(TextField, {
-      variant: "outlined",
-      required: true,
+    }, /* @__PURE__ */ React.createElement("input", {
+      type: "text",
       name: "key",
-      style: {
-        alignSelf: "center",
-        flex: "1 1 auto"
-      },
-      placeholder: "Enter Key",
       value: x.key,
+      placeholder: "Enter Key",
+      required: true,
+      className: "input w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500",
+      style: {alignSelf: "center", flex: "1 1 auto"},
       onKeyDown: (event) => {
         if (!ALPHA_NUMERIC_DASH_REGEX.test(event.key)) {
           event.preventDefault();
@@ -71,16 +78,16 @@ const DynamicInput = ({data, updateData, runQuery}) => {
         marginRight: "10px",
         marginLeft: "10px"
       }
-    }, ":"), /* @__PURE__ */ React.createElement(TextField, {
-      style: {
-        flex: "1 1 auto"
-      },
-      multiline: true,
+    }, ":"), /* @__PURE__ */ React.createElement("textarea", {
       name: "value",
       placeholder: "Enter Value",
-      variant: "outlined",
       value: x.value,
-      onChange: (e) => handleInputChange(e, i)
+      onChange: (e) => {
+        handleInputChange(e, i);
+        adjustHeightTextarea(e.target);
+      },
+      className: "textarea w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2\n                focus:ring-blue-500",
+      style: {flex: "1 1 auto", minHeight: "2.5rem", height: "auto"}
     }), /* @__PURE__ */ React.createElement("div", {
       style: stylesDynamicInput.btn_box
     }, inputList().length !== 1 && /* @__PURE__ */ React.createElement(FiTrash, {
