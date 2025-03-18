dc4a7fa432e82052475e69989f4ba65779e74ecb
yogasw <ariokidev@gmail.com>
2025-03-18 02:23:58 +0000

    Deploying to gh-pages from @ yogasw/tooljet-custom-component@66719cc3032507dae4d7f3381f2975e02bbdca8b 🚀

--- components/conversation-history.js
+++ components/conversation-history.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect, useCallback} from "https://cdn.jsdelivr.net/npm/react@18.2.0/+esm";
 import moment from "https://cdn.jsdelivr.net/npm/moment@2.30.0/+esm";
-import {LuFileJson2} from "https://cdn.jsdelivr.net/npm/react-icons@5.0.0/lu/+esm";
+import {LuFileJson2} from "https://cdn.jsdelivr.net/npm/react-icons@4/lu/+esm";
 const HeadConversationHistory = () => /* @__PURE__ */ React.createElement("head", null, /* @__PURE__ */ React.createElement("link", {
   href: "https://cdn.jsdelivr.net/npm/tailwindcss@v2.2.19/dist/tailwind.min.css",
   rel: "stylesheet"
