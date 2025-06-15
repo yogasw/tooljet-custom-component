051ae3bb3e61290d901ea59be5a140e91aac7f4f
Yoga Setiawan <yogainformatika@gmail.com>
2025-06-16 00:01:08 +0700

    add title

--- src/components/vis-timeline.jsx
+++ src/components/vis-timeline.jsx
@@ -1,11 +1,11 @@
 import React from 'https://cdn.jsdelivr.net/npm/react@18.2.0/+esm';
-import {DataSet, Timeline, Graph2d} from 'https://cdn.jsdelivr.net/npm/vis-timeline@7.7.3/standalone/+esm';
+import { DataSet, Timeline, Graph2d } from 'https://cdn.jsdelivr.net/npm/vis-timeline@7.7.3/standalone/+esm';
 /*for tooljet*/
-import {createRoot} from 'https://cdn.jsdelivr.net/npm/react-dom@18.2.0/+esm';
+import { createRoot } from 'https://cdn.jsdelivr.net/npm/react-dom@18.2.0/+esm';
 
 class GroupTemplate extends React.Component {
     render() {
-        var {group} = this.props;
+        var { group } = this.props;
         return <div>
             <label>{group.content}</label>
         </div>
@@ -14,7 +14,7 @@ class GroupTemplate extends React.Component {
 
 class ItemTemplate extends React.Component {
     render() {
-        var {item} = this.props;
+        var { item } = this.props;
         return <div>
             <label>{item.content}</label>
         </div>
@@ -33,8 +33,8 @@ class VisTimeline extends React.Component {
     initData() {
         var groups = new DataSet()
         var items = new DataSet();
-        const {items: _items, groups: _groups, itemGraph2d: _itemGraph2d, groupGraph2d: _groupGraph2d} = this.props?.data ? this.props?.data : {items: [], groups: []};
-        
+        const { items: _items, groups: _groups, itemGraph2d: _itemGraph2d, groupGraph2d: _groupGraph2d } = this.props?.data ? this.props?.data : { items: [], groups: [] };
+
         if (_groups) {
             _groups.forEach((group) => {
                 groups.add(group)
@@ -51,21 +51,21 @@ class VisTimeline extends React.Component {
         var graph2dData = new DataSet();
         var graph2dGroups = new DataSet();
         var hasGraph2dData = false;
-        
+
         if (_itemGraph2d && _itemGraph2d.length > 0) {
             _itemGraph2d.forEach((item) => {
                 graph2dData.add(item);
             });
             hasGraph2dData = true;
         }
-        
+
         if (_groupGraph2d && _groupGraph2d.length > 0) {
             _groupGraph2d.forEach((group) => {
                 graph2dGroups.add(group);
             });
         }
 
-        return {items, groups, graph2dData, graph2dGroups, hasGraph2dData};
+        return { items, groups, graph2dData, graph2dGroups, hasGraph2dData };
     }
 
     initTimeline(runQuery) {
@@ -96,12 +96,12 @@ class VisTimeline extends React.Component {
             animation: false,
         }
 
-        const {items, groups, graph2dData, graph2dGroups, hasGraph2dData} = this.initData();
-        
+        const { items, groups, graph2dData, graph2dGroups, hasGraph2dData } = this.initData();
+
         // Create Timeline
         var container = document.getElementById('visualization');
         this.timeline = new Timeline(container, items, groups, options);
-        
+
         // Create Graph2d only if there's data
         if (hasGraph2dData) {
             var graph2dContainer = document.getElementById('graph2d');
@@ -112,7 +112,7 @@ class VisTimeline extends React.Component {
                 this.timeline.on('rangechange', (properties) => {
                     if (this.graph2d && !this._updating) {
                         this._updating = true;
-                        this.graph2d.setWindow(properties.start, properties.end, {animation: false});
+                        this.graph2d.setWindow(properties.start, properties.end, { animation: false });
                         this._updating = false;
                     }
                 });
@@ -120,7 +120,7 @@ class VisTimeline extends React.Component {
                 this.graph2d.on('rangechange', (properties) => {
                     if (this.timeline && !this._updating) {
                         this._updating = true;
-                        this.timeline.setWindow(properties.start, properties.end, {animation: false});
+                        this.timeline.setWindow(properties.start, properties.end, { animation: false });
                         this._updating = false;
                     }
                 });
@@ -129,12 +129,12 @@ class VisTimeline extends React.Component {
 
         this.timeline.on('doubleClick', onDoubleClick);
 
-        function onDoubleClick({item: i}) {
+        function onDoubleClick({ item: i }) {
             console.log(runQuery)
             if (!i) return;
             let item = items.get(i);
             if (runQuery) {
-                runQuery("subscribe_double_click", {item: item})
+                runQuery("subscribe_double_click", { item: item })
             }
         }
     }
@@ -152,14 +152,17 @@ class VisTimeline extends React.Component {
     }
 
     render() {
-        const {itemGraph2d, groupGraph2d} = this.props?.data || {};
+        const { itemGraph2d, groupGraph2d } = this.props?.data || {};
+        const { title, titleGraph2d } = this.props;
         const hasGraph2dData = (itemGraph2d && itemGraph2d.length > 0) || (groupGraph2d && groupGraph2d.length > 0);
-        
+
         return <div>
-            <div style={{marginBottom: '20px'}}>
+            <div style={{ marginBottom: '20px' }}>
+                {title && <h2>{title}</h2>}
                 <div id="visualization"></div>
             </div>
-            <div style={{display: hasGraph2dData ? 'block' : 'none'}}>
+            <div style={{ display: hasGraph2dData ? 'block' : 'none' }}>
+                {titleGraph2d && <h2>{titleGraph2d}</h2>}
                 <div id="graph2d"></div>
             </div>
         </div>
